254f207dbb40d0eca1c8870c7df6df2f1f298914
matthieuDev <matthieu.devaux.epfl@outlook.com>
2023-05-17 15:49:35 +0200

    get_info_vote_by_month

--- fifa_summary.R
+++ fifa_summary.R
@@ -1,5 +1,15 @@
 library(dplyr)
 
+get_info_vote_by_month <- function() {
+  fifa %>%
+    mutate(month_year = format(created, "%B %Y")) %>%
+    select(created, month_year, voted_up) %>%
+    group_by(month_year) %>%
+    summarise(nb_votes=length(voted_up), ratio_upvotes=mean(voted_up), created=first(created)) %>%
+    arrange(created) %>%
+    select(created=month_year, nb_votes, ratio_upvotes)
+}
+
 how_many_bad_present <- function() {
   fifa %>%
     filter(recieved_for_free) %>%
